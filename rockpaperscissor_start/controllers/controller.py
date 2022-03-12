54f20704fc7fbaba1b5ac5726eede215cfd321a4
elicodes89 <elicodes89@gmail.com>
2022-03-12 13:07:33 +0000

    managed to change route and display players choices and the result. ALso added a Rules section

--- rockpaperscissor_start/controllers/controller.py
+++ rockpaperscissor_start/controllers/controller.py
@@ -1,21 +1,21 @@
+from flask import render_template, request
 from app import app
 from models.game import lets_play
+from models.player import Player
 
+
+
+# @app.route('/<player1choice>/<player2choice>')
+# def juego(player1choice, player2choice):
+#     player1 = Player("Player1", player1choice)
+#     player2 = Player("Player2", player2choice)
+#     return lets_play(player1, player2)
+    # return player2choice
 
 @app.route('/<player1choice>/<player2choice>')
 def juego(player1choice, player2choice):
-    return lets_play(player1choice, player2choice)
-    # return player2choice
+    player1 = Player("Player1", player1choice)
+    player2 = Player("Player2", player2choice)
+    result = lets_play(player1, player2)
 
-# @app.route('/tasks')
-# def index():
-#     return render_template('index.html', title='Home', tasks=tasks)
-
-# @app.route('/tasks', methods=['POST'])
-# def add_task():
-#     task_title = request.form['title']
-#     task_desc = request.form['description']
-#     new_task = Task(task_title, task_desc, False)
-#     add_new_task(new_task)
-
-#     return render_template('index.html', title='Home', tasks=tasks)
+    return render_template('index.html', title='Home', player1=player1, player2=player2, result=result)
--- rockpaperscissor_start/models/game.py
+++ rockpaperscissor_start/models/game.py
@@ -1,21 +1,21 @@
 # class Game():
 
-# def __init__(self,player_1, player_2):
-#         self.player_1 = player_1
-#         self.player_2 = player_2
+def __init__(self,player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
 
-def lets_play(player1choice, player2choice):
-        if player1choice == "paper" and player2choice == "scissors":
+def lets_play(player1, player2):
+        if player1.choice == "paper" and player2.choice == "scissors":
             return "Player 2 wins!"
-        elif player1choice == "scissors" and player2choice == "rock":
+        elif player1.choice == "scissors" and player2.choice == "rock":
             return "Player 2 wins"
-        elif player1choice == "rock" and player2choice == "paper":
+        elif player1.choice == "rock" and player2.choice == "paper":
             return "Player 2 wins"
-        elif player1choice == "scissors" and player2choice == "paper":
+        elif player1.choice == "scissors" and player2.choice == "paper":
             return "Player 1 wins"
-        elif player1choice == "rock" and player2choice == "scissors":
+        elif player1.choice == "rock" and player2.choice == "scissors":
             return "Player 1 wins"
-        elif player1choice == "paper" and player2choice == "rock":
+        elif player1.choice == "paper" and player2.choice == "rock":
             return "Player 1 wins"
-        elif player1choice == player2choice:
-            return "Oops, try again!"
+        elif player1.choice == player2.choice:
+            return "None"
