83d1c8963e3017a753eb5476ddb04252b658abeb
elicodes89 <elicodes89@gmail.com>
2022-03-14 14:35:02 +0000

    fixed class game.py

--- rockpaperscissor_start/controllers/controller.py
+++ rockpaperscissor_start/controllers/controller.py
@@ -1,7 +1,8 @@
 from flask import render_template, request
 from app import app
-from models.game import lets_play
+from models.game import Game
 from models.player import Player
+import random
 
 @app.route('/rules')
 def rules():
@@ -11,6 +12,7 @@ def rules():
 def juego(player1choice, player2choice):
     player1 = Player("Player1", player1choice)
     player2 = Player("Player2", player2choice)
-    result = lets_play(player1, player2)
+    game = Game()
+    result = game.lets_play(player1, player2)
 
     return render_template('index.html', title='Home', player1=player1, player2=player2, result=result)
--- rockpaperscissor_start/models/game.py
+++ rockpaperscissor_start/models/game.py
@@ -1,11 +1,10 @@
-# class Game():
+class Game():
 
-def __init__(self,player_1, player_2):
-        self.player_1 = player_1
-        self.player_2 = player_2
+    def __init__(self):
+        pass
 
 
-def lets_play(player1, player2):
+    def lets_play(self, player1, player2):
         if player1.choice == "paper" and player2.choice == "scissors":
             return "Player 2 wins!"
         elif player1.choice == "scissors" and player2.choice == "rock":
